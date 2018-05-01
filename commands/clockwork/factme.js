242117c8aec42a00f55735b4967a9af78033b53b
KentaiP <Kentai92@gmail.com>
2018-05-01 17:15:21 +0200

    Update factme.js

--- commands/clockwork/factme.js
+++ commands/clockwork/factme.js
@@ -17,24 +17,19 @@ class FactOfTheDay extends Discord.Command
 
 		var scoopchannel = [];
 	
-	
 			message.guild.channels.get("408726317970751490").fetchMessages().then(messages => 
 			{
 			scoopchannel.push.apply(messages);	
-			var fetchlength = messages.length; 
+			var fetchlength = Array.from(messages).length; 
 			message.channel.send(fetchlength);
 			}).catch(err => {
                			console.log(err.stack);
             			});	
 						
-		
-	
                 var regmatches = Array.from(scoopchannel).toString().match(/#*```(.*?)```/g);
                 var getfact = "There was an uncashed Exception, sorry."; 
                 var exceptions = ["436573366842032128", "434514788966465546"];
                 var cashsearchnone = 0;
-
-				
 				
                 if (args.length > 2)
                 {

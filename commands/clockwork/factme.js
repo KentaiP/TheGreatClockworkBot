a7681ba5a644c8d10f52665830b81bcc1d258209
KentaiP <Kentai92@gmail.com>
2018-05-01 16:35:42 +0200

    Update factme.js

--- commands/clockwork/factme.js
+++ commands/clockwork/factme.js
@@ -32,6 +32,8 @@ class FactOfTheDay extends Discord.Command
                 var getfact = "There was an uncashed Exception, sorry."; 
                 var exceptions = ["436573366842032128", "434514788966465546"];
                 var cashsearchnone = 0;
+
+				message.channel.send(regmatches[0]);
 				
                 if (args.length > 2)
                 {

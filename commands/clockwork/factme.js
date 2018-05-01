0f9adff68a7298f9b9d90d2c8fc2a9e0509607fa
KentaiP <Kentai92@gmail.com>
2018-05-01 21:21:18 +0200

    Update factme.js

--- commands/clockwork/factme.js
+++ commands/clockwork/factme.js
@@ -33,6 +33,47 @@ class FactOfTheDay extends Discord.Command
 							else
 							{
 								var regmatches = messages.toString().match(/#*```(.*?)```/g);
+								var getfact = "There was an uncashed Exception, sorry."; 
+								var exceptions = ["436573366842032128", "434514788966465546"];
+								var cashsearchnone = 0;
+								
+								if (args.length > 2)
+									{
+										var targetedsearch = "Oh, sadly I found nothing, try a different search term.";
+										regmatches.forEach(function(item, index, arr) 
+										{
+										
+										if (item.toLowerCase().indexOf(args.toLowerCase()) !=-1)
+											{
+												if (cashsearchnone == 0)
+												{
+													targetedsearch = "I found the following facts that contain the key word **" + args + "**: \n";
+												}
+												
+												message.channel.send(targetedsearch + item); 
+												targetedsearch = "";
+												cashsearchnone = 1;
+											}
+											
+										});
+
+										if (cashsearchnone == 0)
+												{
+												cashsearchnone = 1;
+												message.channel.send("Oh, sadly I found nothing, try a different search term.");
+												}
+									}
+									else
+									{ 
+										do 
+										{
+											var getfact = regmatches[Math.floor(Math.random() * Math.floor(regmatches.length))];
+											/* console.log(regmatches); */
+										}while(new RegExp(exceptions.join("|")).test(getfact));
+									
+										message.channel.send("Did you know this?" + getfact);
+									} 
+								
 								message.channel.send(regmatches.length);
 							}
 						}).catch(err => {
@@ -51,7 +92,45 @@ class FactOfTheDay extends Discord.Command
 							else
 							{
 								var regmatches = scoopchannel.toString().match(/#*```(.*?)```/g);
-								message.channel.send(regmatches.length);
+								var getfact = "There was an uncashed Exception, sorry."; 
+								var exceptions = ["436573366842032128", "434514788966465546"];
+								var cashsearchnone = 0;
+								if (args.length > 2)
+										{
+											var targetedsearch = "Oh, sadly I found nothing, try a different search term.";
+											regmatches.forEach(function(item, index, arr) 
+											{
+											
+											if (item.toLowerCase().indexOf(args.toLowerCase()) !=-1)
+												{
+													if (cashsearchnone == 0)
+													{
+														targetedsearch = "I found the following facts that contain the key word **" + args + "**: \n";
+													}
+													
+													message.channel.send(targetedsearch + item); 
+													targetedsearch = "";
+													cashsearchnone = 1;
+												}
+												
+											});
+
+											if (cashsearchnone == 0)
+													{
+													cashsearchnone = 1;
+													message.channel.send("Oh, sadly I found nothing, try a different search term.");
+													}
+										}
+										else
+										{ 
+											do 
+											{
+												var getfact = regmatches[Math.floor(Math.random() * Math.floor(regmatches.length))];
+												/* console.log(regmatches); */
+											}while(new RegExp(exceptions.join("|")).test(getfact));
+										
+											message.channel.send("Did you know this?" + getfact);
+										} 
 							}
 						
 						}).catch(err => {
@@ -62,49 +141,6 @@ class FactOfTheDay extends Discord.Command
 			
 			crawlmessages("none", scoopchannel);
 			
-					
-                var regmatches = scoopchannel.toString().match(/#*```(.*?)```/g);
-                var getfact = "There was an uncashed Exception, sorry."; 
-                var exceptions = ["436573366842032128", "434514788966465546"];
-                var cashsearchnone = 0;
-				
-                if (args.length > 2)
-                {
-                    var targetedsearch = "Oh, sadly I found nothing, try a different search term.";
-                    regmatches.forEach(function(item, index, arr) 
-                    {
-                    
-                    if (item.toLowerCase().indexOf(args.toLowerCase()) !=-1)
-                        {
-                            if (cashsearchnone == 0)
-                            {
-                                targetedsearch = "I found the following facts that contain the key word **" + args + "**: \n";
-                            }
-                            
-                            message.channel.send(targetedsearch + item); 
-                            targetedsearch = "";
-                            cashsearchnone = 1;
-                        }
-                        
-                    });
-
-                    if (cashsearchnone == 0)
-                            {
-                            cashsearchnone = 1;
-                            message.channel.send("Oh, sadly I found nothing, try a different search term.");
-                            }
-                }
-                else
-                { 
-                    do 
-                    {
-                        var getfact = regmatches[Math.floor(Math.random() * Math.floor(regmatches.length))];
-                        /* console.log(regmatches); */
-                    }while(new RegExp(exceptions.join("|")).test(getfact));
-                
-                    message.channel.send("Did you know this?" + getfact);
-                }          
-        
     }
 }
 

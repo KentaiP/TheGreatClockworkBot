1e7bc33a50e0ae50a3e3ddd6588eacdab8e0fdf0
KentaiP <Kentai92@gmail.com>
2018-05-01 17:29:59 +0200

    Update factme.js

--- commands/clockwork/factme.js
+++ commands/clockwork/factme.js
@@ -22,7 +22,7 @@ class FactOfTheDay extends Discord.Command
 					{
 					scoopchannel.push.apply(Array.from(messages));	
 					var fetchlength = Array.from(messages).length; 
-					message.channel.send(fetchlength);	
+					message.channel.send(scoopchannel[0]);	
 					}).catch(err => {
 								console.log(err.stack);
 								});
